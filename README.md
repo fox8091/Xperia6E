d378472b900fcfab43e2aabb6e94ca06e052d12b
fox8091 <bfox200012@gmail.com>
2023-02-19 02:40:15 -0600

    Add module

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Xperia6E
-Xperia 1 IV WiFi 6E enabler
+
+Xperia6E is a Magisk module to enable WiFi 6E on non-US Xperia 1 IV models. It accomplishes this by changing a few WiFi-related system props that differ between US and SEA firmwares
---
+++ post-fs-data.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+resetprop -n ro.vendor.sony.wlan.6e_cc_list US
+resetprop -n ro.vendor.product.rf.id PDX223-A2US
+resetprop -n ro.boot.wificountrycode US
+resetprop -n ro.oem_ant_switch_index_table 0,0,2,0,0,2,2,0,2,2,2,2,2,2,2,2,0,0,2,0,0,2,2,0,2,2,2,2,2,2,2,2,0,0,2,0,0,2,2,0,2,2,2,2,2,2,2,2,0,0,2,0,0,2,2,0,2,2,2,2,2,2,2,2
+resetprop -n ro.oem_sar_back_off_index_table 1,2,8,1,6,3,8,1,5,2,8,1,5,2,8,1,1,2,8,1,6,3,8,1,5,2,8,1,5,2,8,1
